32f6da3904752deaf03b55112169ac9504063687
michalkedzia <222463@edu.p.lodz.pl>
2021-04-12 23:46:40 +0200

    Test : order confirm and submit with same date

--- src/test/java/edu/iis/mto/time/OrderTest.java
+++ src/test/java/edu/iis/mto/time/OrderTest.java
@@ -1,17 +1,36 @@
 package edu.iis.mto.time;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
 
+@ExtendWith(MockitoExtension.class)
 class OrderTest {
+  @Mock private Clock clock;
+  private Order order;
 
   @BeforeEach
-  void setUp() throws Exception {}
+  void setUp() throws Exception {
+    order = new Order(clock);
+  }
 
   @Test
-  void test() {
-    fail("Not yet implemented");
+  void order_should_be_confirmed_with_same_order_date() {
+    Instant orderSubmit = Instant.parse("2021-04-12T21:35:23.084569400Z");
+    Instant orderConfirm = orderSubmit.plus(0, ChronoUnit.HOURS);
+    Mockito.when(clock.getZone()).thenReturn(ZoneId.systemDefault());
+    Mockito.when(clock.instant()).thenReturn(orderSubmit).thenReturn(orderConfirm);
+    order.submit();
+    order.confirm();
+    Assertions.assertSame(order.getOrderState(), Order.State.CONFIRMED);
   }
 }
